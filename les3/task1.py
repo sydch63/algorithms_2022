fb663f8610c3afc7c95d325546cf4731e7bfa379
sydch <sid.pasch@gmail.com>
2024-09-16 13:26:06 +0400

    done task1

--- les3/task1.py
+++ les3/task1.py
@@ -14,26 +14,27 @@ def dec_time(func):
     def wrapper(*args, **kwargs):
         start = time.time()
         time.sleep(1)
-        print((func(*args, **kwargs)))
+        result = func(*args, **kwargs)
         end = time.time()
         print(f'Затраченное время: {end - start}')
+        return result
     return wrapper
 
-#@dec_time
+@dec_time
 def lst_gen(k,n,lst=[]):
     for i in range(k,n):
         lst.append(i)
     return lst
 
-#@dec_time
+@dec_time
 def dct_gen(k,n,dct={}):
     for i in range(k,n):
         dct.setdefault(i,i)
     return dct
 
 
-#lst = lst_gen(0,10)
-#dct = dct_gen(0,10)
+lst = lst_gen(0,10)
+dct = dct_gen(0,10)
 """
 b) получение элемента списка, оцените сложность в O-нотации (операции нужно провдить в цикле)
    получение элемента словаря, оцените сложность в O-нотации (операции нужно провдить в цикле)
@@ -76,3 +77,21 @@ put_dct(3,0,15)
 обязательно реализуйте ф-цию-декоратор и пусть она считает время
 И примените ее к своим функциям!
 """
+
+@dec_time
+def pop_lst(a=3,b=0,c=0):
+    lst = lst_gen(b,c,[])
+    for i in range(len(lst)):
+        if i == a:
+            return print(lst.pop(i)),print(lst)
+
+pop_lst(3,0,15)
+
+@dec_time
+def pop_dct(a=3,b=0,c=0):
+    dct = dct_gen(0,15,{})
+    for i in dct.keys():
+        if i == a:
+            return print(dct.pop(i)),print(dct)
+
+pop_dct(3,0,15)
